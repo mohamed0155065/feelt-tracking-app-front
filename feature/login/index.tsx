d4eb26b3ed81a70447dea2d111d1e4fb6b01179c
Ahdammar <ahdammar66@gmail.com>
2026-07-10 05:11:04 +0300

    feat: implement smart password visibility and clean layout

---
+++ feature/login/index.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+import DotBackground from './DotBackground';
+import LoginForm from './LoginForm';
+
+export default function LoginFeature() {
+  return (
+    <DotBackground>
+      <LoginForm />
+    </DotBackground>
+  );
+}
